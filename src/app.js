b2dede4102c72554beddf94575f2212d7d254b7a
Melody <melodysamuel126@gmail.com>
2025-10-14 03:21:58 +0100

    fix(auth): resolve route registration issue and connect controllers correctly

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import authRoutes from "../src/routes/auth.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 
 
@@ -21,5 +21,11 @@ app.use("/api/auth", authRoutes);
 // Database
 connectDB();
 
+app.get("/", (req, res) => {
+    res.send("Zorah API is running");
+  });
+  
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+ 
