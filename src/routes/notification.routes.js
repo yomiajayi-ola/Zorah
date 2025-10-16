383d35b943fbdaa1745ca12a15238d8409c68e25
Melody <melodysamuel126@gmail.com>
2025-10-16 09:34:09 +0100

    feat(notification): get notification)

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
 import {
     getNotifications,
+    markAsRead,
 } from "../controllers/notificationController.js"
 
 const router = express.Router();
 
 router.get("/get-not", protect, getNotifications);
+router.get("/:id/read", protect, markAsRead)
 
 export default router;
