1e09edf1615dbc91c33d95604ad330c608bc47eb
Melody <melodysamuel126@gmail.com>
2025-10-21 13:14:07 +0100

    feat(savingsGoal): add savingSgoal model

---
+++ src/models/SavingsGoal.js
@@ -0,0 +1,15 @@
+// import { required } from "joi";
+import mongoose from "mongoose";
+import { CreatedAt } from "sequelize-typescript";
+
+const savingsGoalSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    targetAmount: { type: Number, required: true },
+    currentAmount: { type: Number, default: 0 },
+    deadline: { type: Date, required: true },
+    description: { type: String },
+    ceatedAt: { type, Date, default: Date.now },
+});
+
+export default mongoose.model("SavingsGoal", savingsGoalSchema);
