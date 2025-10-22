f7b562e996fd353d1a622f542615e90f8594ead4
Melody <melodysamuel126@gmail.com>
2025-10-22 10:55:01 +0100

    feat(esusu): add join group)

--- src/controllers/esusuController.js
+++ src/controllers/esusuController.js
@@ -20,3 +20,24 @@ export const createGroup = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+// @desc Join existing Esusu group 
+export const joinGroup = async (req, res) => {
+    try {
+        const { groupId } = req.body;
+        const group = await EsusuGroup.findById(groupId);
+        if (!group) return res.status(404).json({ message: "Group not found"});
+
+        // Check if already a member 
+        if (group.members.some(members => members.user.toString() === req.user._id.toString())) {
+            return res.status(400).json({ message: "Already a member" });
+        }
+
+        group.members.push({ user: req.user._id, order: group.members.length + 1 });
+        await group.save();
+
+        res.json({ message: "Joined successfully", group });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- src/routes/esusu.routes.js
+++ src/routes/esusu.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
-import { createGroup } from "../controllers/esusuController.js";
+import { createGroup, joinGroup } from "../controllers/esusuController.js";
 
 const router = express.Router();
 
 router.post("/create", protect, createGroup);
+router.post("/join", protect, joinGroup);
+
 
 
 
