d71745c153ec9b1528c8217a47369844f5dd7e5f
Melody <melodysamuel126@gmail.com>
2025-11-11 14:38:41 +0100

    feat(income): add Income Model

---
+++ src/models/Income.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const incomeSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    source: {
+      type: String,
+      required: true, // e.g. “Salary”, “Freelance”, “Investment”
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    description: {
+      type: String,
+    },
+    category: {
+      type: String,
+      enum: ["salary", "freelance", "business", "gift", "investment", "other"],
+      default: "other",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Income", incomeSchema);
