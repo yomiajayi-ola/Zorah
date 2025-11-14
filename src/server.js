66ab9ffe893a92c0b8566f3a4ca0839cdd2a69ad
Melody <melodysamuel126@gmail.com>
2025-11-14 07:01:56 +0100

    feat(Esusu Payout): comment cron

--- src/server.js
+++ src/server.js
@@ -31,4 +31,4 @@ app.post("/send-notification", async (req, res) => {
 const PORT = 4000;
 app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
 
-import "./cron/payoutCron.js"
+// import "./cron/payoutCron.js"
