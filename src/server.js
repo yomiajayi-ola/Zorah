59a5cd23065cd01dde9f8e440ef6b10ae2f78de5
Melody <melodysamuel126@gmail.com>
2025-12-12 16:21:05 +0100

    feat(routes): update entry file

--- src/server.js
+++ src/server.js
@@ -28,7 +28,4 @@ app.post("/send-notification", async (req, res) => {
   res.json(data);
 });
 
-const PORT = 4000;
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
-
 // import "./cron/payoutCron.js"
