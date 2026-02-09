be4627f5408417db4d83618f158735ad990c32a1
Melody <melodysamuel126@gmail.com>
2026-02-09 17:12:56 +0100

    fix(core) email error:

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,12 +3,14 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (options) => {
     const smtpConfig = {
         host: process.env.SMTP_HOST || "smtp.gmail.com", 
-        port: Number(process.env.SMTP_PORT) || 465,
+        port: Number(process.env.SMTP_PORT) || 587,
         secure: Number(process.env.SMTP_PORT) === 465 || true, 
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS, 
         },
+        logger: true, 
+        debug: true,
         tls: {
             
             rejectUnauthorized: false 
